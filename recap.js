58dada626ce42c1cca27b77124ddda3957a22649
evo264714 <mh264714@gmail.com>
2023-01-30 21:19:36 +0600

    first

---
+++ recap.js
@@ -0,0 +1,10 @@
+//* 1. Variable
+var myName = 'Evo';
+
+//* Math Operation: +, -, *, /
+//* Shorthand: +=, -=, *=, /=
+//* ++, --
+
+//* 2. Array
+var friends = ['Messi', 'Neymar', 'Ronaldo', 'Salah', 'Suarez'];
+
